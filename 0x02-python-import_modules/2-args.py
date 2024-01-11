6d321d5c234500c6c3af49aeb2131e859374d6b1
IgweEmmanuel <chigozieigweemmanuel@gmail.com>
2024-01-11 04:24:51 +0000

    data structure

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -2,12 +2,12 @@
 from sys import argv
 if __name__ != "__main__":
     exit()
-    x = len(argv) - 1
-    if x < 1:
-        print("{} argument:".format(x))
-    elif x == 1:
-        print("{} argument:".format(x))
-    else:
-        print("{} arguments:".format(x))
-    for i in range(x):
-        print("{}: {:s}".format(i + 1, argv[i + 1]))
+x = len(argv) - 1
+if x < 1:
+    print("{} argument:".format(x))
+elif x == 1:
+    print("{} argument:".format(x))
+else:
+    print("{} arguments:".format(x))
+for i in range(x):
+    print("{}: {:s}".format(i + 1, argv[i + 1]))
