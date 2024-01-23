36d30d837540f22b261b0bbe8fe467c6d087ed3a
IgweEmmanuel <chigozieigweemmanuel@gmail.com>
2024-01-23 04:43:58 +0000

    exceptions

---
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+def safe_print_integer(value):
+    """Prints an integer.
+    
+    Args:
+        value (int): The integer to print
+    
+    Return:
+        TypeError or ValueError occurs is - False.
+        Else it is - True
+    """
+
+    try:
+        print("{:d}".format(value)
+            return (true)
+    except (TypeError, ValueError):
+    return (False)
