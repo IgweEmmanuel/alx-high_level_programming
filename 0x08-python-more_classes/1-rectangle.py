6039f395360364e5dfd562ae11e513fbf181c775
IgweEmmanuel <chigozieigweemmanuel@gmail.com>
2024-01-30 10:38:45 +0000

    empty class Rectangle created

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -20,10 +20,14 @@ class Rectangle:
 
     @property
     def width(self):
+        """Gets the width
+        """
         return self.__width
 
     @width.setter
     def width(self, value):
+        """Sets the width value
+        """
         if not type(value) is int:
             raise TypeError("width must be an integer")
         if value < 0:
@@ -32,10 +36,14 @@ class Rectangle:
 
     @property
     def height(self):
+        """Gets the height
+        """
         return self.__height
 
     @height.setter
     def height(self, value):
+        """Sets the height value
+        """
         if not type(value) is int:
             raise TypeError("height must be an integer")
         if value < 0:
