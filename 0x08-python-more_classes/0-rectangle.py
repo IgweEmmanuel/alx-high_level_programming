a22647cfe17201f0292e59c761adfa624bff82fe
IgweEmmanuel <chigozieigweemmanuel@gmail.com>
2024-01-30 04:54:53 +0000

    empty class Rectangle created

--- 0x08-python-more_classes/0-rectangle.py
+++ 0x08-python-more_classes/0-rectangle.py
@@ -1,6 +1,11 @@
 #!/usr/bin/python3
 """This is to create a simple rectangle"""
 class Rectangle:
+    """
+        This class returns rectangle
+        Return:
+            it returns empty class
+    """
     pass
 
 
---
+++ 0x08-python-more_classes/1-rectangle.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""REal definition of a rectangle"""
+class Rectangle:
+    """
+        The code defines a rectangle
+        Args:
+            width - this is the width of rectangls
+            height - this is the height of rectangle
+        Return:
+            This returns int
+    """
+    def __init__(self, width=0, height=0):
+        self.width = width
+        self.height = height
+
+    def result(self):
+        if self.name != int(self.name):
+            try:
+                print()
+            except TyeError:
+                print()
+    def set_width(self, width):
+        self.width = width
+    def get_width(self):
+        return self.width
+    def set_height(self, height):
+        self.height = height
+    def get_height(self):
+        return self.height
+
+
