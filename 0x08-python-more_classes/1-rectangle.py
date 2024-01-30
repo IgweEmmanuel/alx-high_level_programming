9dda691d7f487f1444a2c847a322047b9d14b1c0
IgweEmmanuel <chigozieigweemmanuel@gmail.com>
2024-01-30 10:10:13 +0000

    empty class Rectangle created

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -13,8 +13,8 @@ class Rectangle:
                 width: this is the width of the rectangle
                 height: this is the height of the rectangle
         """
-        self.__height = height
-        self.__width = width
+        self.height = height
+        self.width = width
 
     @property
     def width(self):
