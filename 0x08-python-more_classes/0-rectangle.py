508c967f107be468e0d6ba546f307f247ce9f889
IgweEmmanuel <chigozieigweemmanuel@gmail.com>
2024-01-30 04:57:03 +0000

    empty class Rectangle created

--- 0x08-python-more_classes/0-rectangle.py
+++ 0x08-python-more_classes/0-rectangle.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 """This is to create a simple rectangle"""
+
 class Rectangle:
     """
-        This class returns rectangle
+        This class returns empty rectangle class
         Return:
             it returns empty class
     """
