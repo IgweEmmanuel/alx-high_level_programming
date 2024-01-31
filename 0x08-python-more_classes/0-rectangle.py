44b0d868c0f9cbcd336639e50c9eee078c37ad63
IgweEmmanuel <chigozieigweemmanuel@gmail.com>
2024-01-31 08:43:11 +0000

    object in python

--- 0x08-python-more_classes/0-rectangle.py
+++ 0x08-python-more_classes/0-rectangle.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python3
 """Creating empty class"""
 
+
 class Rectangle:
-    
+
     """
         This is an empty class created
         Attribute:
             pass: this is the attribute statement
     """
-    
+
     pass
