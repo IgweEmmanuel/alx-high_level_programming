7075abc82405958c1fb92c5f50255772ede2c1e6
IgweEmmanuel <chigozieigweemmanuel@gmail.com>
2024-02-05 21:27:38 +0000

    returns the list of attributes and methods

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -7,9 +7,10 @@ class MyList(list):
     This subclass prints the list
     """
 
-
     def print_sorted(self):
         """This public instance prints the list
+        Args:
+            self: this is the instance of the object
+        Return: this returns the sorted list
         """
         print(sorted(self))
-
