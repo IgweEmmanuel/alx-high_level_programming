cd14ea39a42be9791c9190156c7c40bf83a009f7
IgweEmmanuel <chigozieigweemmanuel@gmail.com>
2024-02-05 23:24:10 +0000

    returns the list of attributes and methods

---
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""The BaseGeometry class"""
+
+
+class BaseGeometry:
+    """
+    Class geometry
+    """
+
+    def area(self):
+        """area
+        Args:
+            self: the object method
+        Return: exception
+        """
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """integer validator
+        Args:
+            self: instance referer
+            name: name
+            value: integer value
+        Return: value
+        """
+        self.name = str
+        if not type(value) is int:
+            raise TypeError(name + " must be an integer")
+        if value <= 0:
+            raise ValueError(name + " must be greater than 0")
