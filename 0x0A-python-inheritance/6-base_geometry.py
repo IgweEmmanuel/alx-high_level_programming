bdecdbba8218a5f3e085513a4b64577e4b083138
IgweEmmanuel <chigozieigweemmanuel@gmail.com>
2024-02-05 22:59:40 +0000

    returns the list of attributes and methods

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""This is geometry class with public instance"""
+
+
+class BaseGeometry:
+    """
+    BaseGeometry
+    """
+    
+    def area(self):
+        """raises exception on areas
+        Args:
+            self: this is instance of object
+        Return: exception
+        """
+        raise Exception("area() is not implemented")
