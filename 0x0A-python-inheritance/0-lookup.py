a5352bf0baedf0bb9fe81e53680833ad848f1812
IgweEmmanuel <chigozieigweemmanuel@gmail.com>
2024-02-05 16:25:59 +0000

    returns the list of attributes and methods

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
-"""This looks up for all the attriubte and methods in a class and prints them"""
+"""This looks up for all the attriubte and
+    methods in a class and prints them"""
 
 
 def lookup(obj):
-    """This is the method that returns teh list of available attrubutes and methods"""
-
+    """This is the method that returns
+    teh list of available attrubutes and methods
+    """
     return [attr for attr in dir(obj)]
