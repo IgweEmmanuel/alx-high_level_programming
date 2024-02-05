f933a9489bad6b7c3d31b08aa8c2726007d1e9e5
IgweEmmanuel <chigozieigweemmanuel@gmail.com>
2024-02-05 22:45:32 +0000

    returns the list of attributes and methods

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -9,7 +9,4 @@ def inherits_from(obj, a_class):
         a_class: class to check for
     Return: true or false
     """
-    if isinstance(obj, a_class):
-        return True
-    else:
-        return False
+    return  issubclass(type(obj), a_class) and type(obj) != a_class
