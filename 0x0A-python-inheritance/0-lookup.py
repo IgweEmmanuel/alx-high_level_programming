faa052c43ec74d40bdfa9f825d2f00ca151bc555
IgweEmmanuel <chigozieigweemmanuel@gmail.com>
2024-02-05 16:20:57 +0000

    returns the list of attributes and methods

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -4,4 +4,6 @@
 
 def lookup(obj):
     """This is the method that returns teh list of available attrubutes and methods"""
-    return [attr for attr in dir(obj) if callable(getattr(obj, attr)) or attr.startswith("__")]
+
+    return [attr for attr in dir(obj)
+            if callable(getattr(obj, attr)) or attr.startswith("__")]
