2f1cda2dcc0b37e31557b8a42841c66ea301b9a3
IgweEmmanuel <chigozieigweemmanuel@gmail.com>
2024-02-06 17:30:50 +0000

    returns the list of attributes and methods

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Python read file"""
+
+
+def read_file(filename=""):
+    """This reads file
+    Args:
+        filename: the name of the file
+    Return: the content of the file
+    """
+    with open(filename, encoding="utf-8") as myFile:
+        myFile.read()
+        
