ef43dfc6cd44872156d725dccd338a3d06970008
IgweEmmanuel <chigozieigweemmanuel@gmail.com>
2024-02-07 11:51:08 +0000

    return JSON representation of an object (string)

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""Python write into file"""
+import json
+
+def to_json_string(my_obj):
+    """json representation of a file
+    Args:
+        my_obj: the text object (string)
+    Return: the serialized file
+    """
+    return json.dumps(my_obj)
