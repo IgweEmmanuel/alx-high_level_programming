c3f862f0cc4125b735b81b087a6fc43cc5f9956f
IgweEmmanuel <chigozieigweemmanuel@gmail.com>
2024-02-07 16:57:32 +0000

    Write a function that creates an Object from a “JSON file”

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -2,11 +2,11 @@
 """Python write into file"""
 import sys
 
-save_to_json_file = __import__("5-save_to_json_file").save_to_json_file
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
-filename = load_from_json_file("add_item.json")
-with open(filename, 'w', encoding='utf-8') as myFile:
-    myFile = []
-myFile.extend(sys.argv[1:])
-save_to_json_file(myFile, "add_item.json")
+filename = load_from_json_file("add_item.json") or []
+
+filename.extend(sys.argv[1:])
+
+save_to_json_file(filename, "add_item.json")
---
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""returns dictionary description"""
+
+
+def class_to_json(obj):
+    """Initialize an empty dictionary to store the JSON representation"""
+    json_dict = {}
+    
+    # Iterate over the attributes of the object
+    for attr_name in dir(obj):
+        # Exclude special attributes (e.g., __class__, __dict__, etc.)
+        if not attr_name.startswith("__"):
+            # Get the value of the attribute
+            attr_value = getattr(obj, attr_name)
+            # Check if the attribute value is serializable
+            if isinstance(attr_value, (int, float, str, bool)):
+                # If serializable, add it to the JSON dictionary
+                json_dict[attr_name] = attr_value
+            elif isinstance(attr_value, list):
+                # If the attribute value is a list, recursively serialize each item
+                json_dict[attr_name] = [class_to_json(item) if hasattr(item, '__dict__') else item for item in attr_value]
+            elif isinstance(attr_value, dict):
+                # If the attribute value is a dictionary, recursively serialize each value
+                json_dict[attr_name] = {key: class_to_json(value) if hasattr(value, '__dict__') else value for key, value in attr_value.items()}
+    
+    return json_dict
+
