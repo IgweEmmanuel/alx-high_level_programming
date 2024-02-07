b770e6747273dc9b803b846c83cfd7b49e6d6950
IgweEmmanuel <chigozieigweemmanuel@gmail.com>
2024-02-07 16:24:39 +0000

    Write a function that creates an Object from a “JSON file”

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python3
 """Python write into file"""
-import json
+import sys
 
-save = __import__("5-save_to_json_file").save_to_json_file
+save_to_json_file = __import__("5-save_to_json_file").save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
-filename = "add_item.json"
-
-json.dump(save(load_from_json_file(filename), filename), filename)
+filename = load_from_json_file("add_item.json")
+with open(filename, 'w', encoding='utf-8') as myFile:
+    myFile = []
+myFile.extend(sys.argv[1:])
+save_to_json_file(myFile, "add_item.json")
