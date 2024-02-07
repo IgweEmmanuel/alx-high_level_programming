c5087621ee6ad2e61f79da898abe2d0005fe7db1
IgweEmmanuel <chigozieigweemmanuel@gmail.com>
2024-02-07 17:00:47 +0000

    Write a function that creates an Object from a “JSON file”

--- 0x0B-python-input_output/8-class_to_json.py
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -17,11 +17,14 @@ def class_to_json(obj):
                 # If serializable, add it to the JSON dictionary
                 json_dict[attr_name] = attr_value
             elif isinstance(attr_value, list):
-                # If the attribute value is a list, recursively serialize each item
-                json_dict[attr_name] = [class_to_json(item) if hasattr(item, '__dict__') else item for item in attr_value]
+                # If the attribute value is a list, recursively serialize list
+                json_dict[attr_name] = [class_to_json(item)
+                        if hasattr(item, '__dict__') else item for item in attr_value]
             elif isinstance(attr_value, dict):
-                # If the attribute value is a dictionary, recursively serialize each value
-                json_dict[attr_name] = {key: class_to_json(value) if hasattr(value, '__dict__') else value for key, value in attr_value.items()}
+                # If the attribute value is a dictionary
+                json_dict[attr_name] = {key: class_to_json(value)
+                        if hasattr(value, '__dict__')
+                        else value for key, value in attr_value.items()}
     
     return json_dict
 
