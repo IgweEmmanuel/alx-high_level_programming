0e555ff20b73d971c86c4d73cc328c9d0c7a8e77
IgweEmmanuel <chigozieigweemmanuel@gmail.com>
2024-03-12 13:50:00 +0000

    update matrix division file

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -9,17 +9,12 @@ def matrix_divided(matrix, div):
         div: this is the divisor
     Returns: This returns a new matrix
     """
-    if not isinstance(matrix, list)
-    or not all(isinstance(row, list) for row in matrix):
-        raise
-        TypeError("matrix must be a matrix (list of lists) of integers/floats")
-    if not
-    all(isinstance(element, (int, float))for row in matrix for element in row):
-        raise
-        TypeError("matrix must be a matrix (list of lists) of integers/floats")
+    if not isinstance(matrix, list) or not all(isinstance(row, list) for row in matrix):
+        raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+    if not all(isinstance(element, (int, float)) for row in matrix for element in row):
+        raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
     if not matrix or not matrix[0]:
-        raise
-        TypeError("matrix must be a matrix (list of lists) of integers/floats")
+        raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
     row_size = len(matrix[0])
     if not all(len(row) == row_size for row in matrix):
         raise TypeError("Each row of the matrix must have the same size")
