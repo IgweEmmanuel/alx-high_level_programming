cfc64bbc7e55cfb18adf08b8591a178845cbf5de
IgweEmmanuel <chigozieigweemmanuel@gmail.com>
2024-03-12 08:03:07 +0000

    updated rectangle calculation

--- 0x08-python-more_classes/6-rectangle.py
+++ 0x08-python-more_classes/6-rectangle.py
@@ -50,7 +50,7 @@ class Rectangle:
 
     def area(self):
         """area of rectangle"""
-        return(self.__height * self.__width)
+        return (self.__height * self.__width)
 
     def perimeter(self):
         """perimeter of rectangle"""
