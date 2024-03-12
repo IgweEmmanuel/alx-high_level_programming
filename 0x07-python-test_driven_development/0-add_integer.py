8bf2fcad596bd504fc1ecc50ff1ae1d118621273
IgweEmmanuel <chigozieigweemmanuel@gmail.com>
2024-03-12 10:30:14 +0000

    update add integer file

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
+"""This wil define integer addition"""
+
+
 def add_integer(a, b=98):
     """This function returns sum of a and b
     Args:
@@ -11,4 +14,4 @@ def add_integer(a, b=98):
         raise TypeError("a must be an integer")
     if not isinstance(b, (int, float)):
         raise TypeError("b must be an integer")
-    return int(a) + int(b)
+    return (int(a) + int(b))
