4c219dfd156d8baf3ee1b1523fb666a36298edd2
IgweEmmanuel <chigozieigweemmanuel@gmail.com>
2024-03-13 17:21:48 +0000

    lists all cities with two tables

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -1,2 +1,7 @@
--- Lists all cities contained in the dataase hbtn_0d_usa
-SELECT cities.id, cities.name, states.name FROM hbtn_0d_usa.states.cities INNER JOIN hbtn_0d_usa.states ON cities.state_id = states.id ORDER BY cities.id;
+-- Lists all cities in the database hbtn_0d_usa.
+-- Records are sorted in order of ascending cities.id.
+SELECT c.`id`, c.`name`, s.`name`
+  FROM `cities` AS c
+       INNER JOIN `states` AS s
+       ON c.`state_id` = s.`id`
+ ORDER BY c.`id`;
