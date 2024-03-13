03fc9226d14e29cd71c741b20473e0b8ea5a2942
IgweEmmanuel <chigozieigweemmanuel@gmail.com>
2024-03-13 12:55:40 +0000

    create user and add all privileges

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,3 +1,3 @@
 -- Create user and add all privileges
 CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
-GRANT PRIVILEGE ON *.* TO 'user_0d_1'@'localhost';
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
