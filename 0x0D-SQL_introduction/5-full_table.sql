336d80e8617b8a2bdedd736a6e1862a3d7cb62af
IgweEmmanuel <chigozieigweemmanuel@gmail.com>
2024-03-12 19:01:13 -0700

    full table description

--- 0x0D-SQL_introduction/5-full_table.sql
+++ 0x0D-SQL_introduction/5-full_table.sql
@@ -1,6 +1,2 @@
--- Print full description of first_table
-SELECT *
-FROM INFORMATION_SCHEMA.COLUMNS
-WHERE TABLE_SCHEMA = `hbtn_0c_0`
-AND TABLE_NAME = `first_table`;
-
+-- Prints the full description of the table first_table.
+SHOW CREATE TABLE `first_table`;
