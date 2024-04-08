973d736770115a7c50390b9f21ca8796014aa12a
IgweEmmanuel <chigozieigweemmanuel@gmail.com>
2024-04-08 23:23:11 +0000

    argument js

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -4,7 +4,7 @@ const argv = process.argv.length - 2;
 if (argv === 0) {
   console.log('No argument');
 } else if (argv === 1) {
-  console.log('Argment found');
+  console.log('Argument found');
 } else {
   console.log('Arguments found');
 }
