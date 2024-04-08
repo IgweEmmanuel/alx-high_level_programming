5577f66f467e644ad15e86e742e43d71a68e8791
IgweEmmanuel <chigozieigweemmanuel@gmail.com>
2024-04-08 23:57:08 +0000

    concat

--- 0x12-javascript-warm_up/4-concat.js
+++ 0x12-javascript-warm_up/4-concat.js
@@ -3,6 +3,8 @@ const [,, ...argv] = process.argv;
 
 if (argv[0] && argv[1]) {
   console.log(argv[0] + ' is ' + argv[1]);
+} else if (argv[0]) {
+  console.log(argv[0] + ' is undefined');
 } else {
-  console.log('');
+  console.log('undefined is undefined');
 }
