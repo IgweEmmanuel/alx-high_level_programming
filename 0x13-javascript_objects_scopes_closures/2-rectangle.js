3d0593cd014f44eb1e7f552d6d8d2d38827a6e0e
IgweEmmanuel <chigozieigweemmanuel@gmail.com>
2024-04-10 03:03:25 +0000

    rectangle 2

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -1,12 +1,10 @@
 #!/usr/bin/node
 class Rectangle {
-  constructor(w, h){
-    if (w <= 0 || h <= 0 || !w || !h){
-      return {};
-    }
+  constructor(w, h) {
+    if (w > 0 && h > 0){
       this.width = w;
       this.height = h;
+    }
   }
 }
-
 module.exports = Rectangle;
