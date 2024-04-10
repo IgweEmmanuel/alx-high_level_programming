5f18291dbc0cecda3ab19fe8a0d971371d8bb32f
IgweEmmanuel <chigozieigweemmanuel@gmail.com>
2024-04-10 09:06:35 +0000

    sorted

--- 0x13-javascript_objects_scopes_closures/101-sorted.js
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -5,11 +5,11 @@ const dict = require('./101-data').dict;
 const invertedDict = {};
 
 for (const userId in dict) {
-    const occurrence = dict[userId];
-    if (!(occurrence in invertedDict)) {
-        invertedDict[occurrence] = [];
-    }
-    invertedDict[occurrence].push(userId);
+  const occurrence = dict[userId];
+  if (!(occurrence in invertedDict)) {
+    invertedDict[occurrence] = [];
+  }
+  invertedDict[occurrence].push(userId);
 }
 
 console.log(invertedDict);
