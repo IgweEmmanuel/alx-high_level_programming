1b24381ba393ca75b30565aa30d0c5724df9f491
IgweEmmanuel <chigozieigweemmanuel@gmail.com>
2024-04-15 19:58:42 +0000

    cities by states

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """
-This script lists all states from the
-database `hbtn_0e_0_usa`.
+This script lists all cities from
+the database `hbtn_0e_4_usa`.
 """
 
 import MySQLdb
@@ -9,30 +9,29 @@ from sys import argv
 
 if __name__ == '__main__':
     """
-    Access to the database and get the states
+    Access to the database and get the cities
     from the database.
     """
+
     db = MySQLdb.connect(host="localhost", user=argv[1], port=3306,
                          passwd=argv[2], db=argv[3])
 
-    cur = db.cursor()
-    cur.execute("""
-                SELECT
-                    cities.id, cities.name, states.name
-                FROM
-                    cities
-                JOIN
-                    states
-                ON
-                    cities.id = states.id
-                ORDER BY
-                    cities.id ASC
-                """)
-    rows = cur.fetchall()
+    with db.cursor() as cur:
+        cur.execute("""
+            SELECT
+                cities.id, cities.name, states.name
+            FROM
+                cities
+            JOIN
+                states
+            ON
+                cities.state_id = states.id
+            ORDER BY
+                cities.id ASC
+        """)
+
+        rows = cur.fetchall()
 
     if rows is not None:
         for row in rows:
             print(row)
-
-    cur.close()
-    db.close()
