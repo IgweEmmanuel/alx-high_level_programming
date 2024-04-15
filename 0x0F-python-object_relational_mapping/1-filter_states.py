7d0479239fa3127761f67a2cf468db56985364e9
IgweEmmanuel <chigozieigweemmanuel@gmail.com>
2024-04-15 15:40:02 +0000

    filter

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,21 +1,33 @@
 #!/usr/bin/python3
+"""
+This script filters the table row
+"""
+
+
 import MySQLdb
 import sys
 
 if __name__ == '__main__':
-if len(sys.argv) != 4:
-    print('You should use this format: {}, user, password, database', format(sys.argv[0]))
+    """
+    This is the filtering section
+    of the code
+    """
+    if len(sys.argv) != 4:
+    print('You should use this format:
+            {}, user, password, database', format(sys.argv[0]))
     sys.exit(1)
 
 username = sys.argv[1]
 password = sys.argv[2]
 database = sys.argv[3]
 
-db = MySQLdb.connect(host='localhost', port=3306, user=username, passwd=password, db=database)
+db = MySQLdb.connect(host='localhost',
+                    port=3306, user=username,
+                    passwd=password, db=database)
 
 cursor = db.cursor
 
-cursor.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC")
+cursor.execute("SELECT * FROM states WHERE name LIKE 'N%'")
 
 states = cursor.fetchall()
 
