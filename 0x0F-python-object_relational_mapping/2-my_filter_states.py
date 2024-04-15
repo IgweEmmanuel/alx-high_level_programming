d76ae4bc720c89e455dd24fb21e71c196c3ae621
IgweEmmanuel <chigozieigweemmanuel@gmail.com>
2024-04-15 18:56:35 +0000

    filter2

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -26,8 +26,8 @@ if __name__ == '__main__':
     cursor = db.cursor()
 
     cursor.execute("SELECT * FROM states\
-                    WHERE name LIKE BINARY 'N%'\
-                    ORDER BY states.id ASC", (statename,))
+                    WHERE name LIKE BINARY %(name)s\
+                    ORDER BY states.id ASC", {'name':statename})
 
     states = cursor.fetchall()
 
