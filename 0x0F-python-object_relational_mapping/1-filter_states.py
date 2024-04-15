d09febae53d2f09c885815c0fac9e86e2caebc59
IgweEmmanuel <chigozieigweemmanuel@gmail.com>
2024-04-15 15:42:01 +0000

    filter

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -5,21 +5,21 @@ This script filters the table row
 
 
 import MySQLdb
-import sys
+from sys import argv
 
 if __name__ == '__main__':
     """
     This is the filtering section
     of the code
     """
-    if len(sys.argv) != 4:
+    if len(argv) != 4:
     print('You should use this format:
-            {}, user, password, database', format(sys.argv[0]))
+            {}, user, password, database', format(argv[0]))
     sys.exit(1)
 
-username = sys.argv[1]
-password = sys.argv[2]
-database = sys.argv[3]
+username = argv[1]
+password = argv[2]
+database = argv[3]
 
 db = MySQLdb.connect(host='localhost',
                     port=3306, user=username,
