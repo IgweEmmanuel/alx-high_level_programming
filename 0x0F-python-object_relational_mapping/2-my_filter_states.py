c8c20b64b9e082c405f3db6fa9f8743783138126
IgweEmmanuel <chigozieigweemmanuel@gmail.com>
2024-04-15 18:52:19 +0000

    filter2

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+"""
+This script filters the table row
+"""
+
+
+import MySQLdb
+from sys import argv
+
+if __name__ == '__main__':
+    """
+    This is the filtering section
+    of the code
+    """
+    username = argv[1]
+    password = argv[2]
+    database = argv[3]
+    statename = argv[4] 
+
+    db = MySQLdb.connect(
+                        host='localhost',
+                        user=username, port=3306,
+                        passwd=password,
+                        db=database, name=statename)
+
+    cursor = db.cursor()
+
+    cursor.execute("SELECT * FROM states\
+                    WHERE name LIKE BINARY 'N%'\
+                    ORDER BY states.id ASC", (statename,))
+
+    states = cursor.fetchall()
+
+    for state in states:
+        print(state)
+
+    cursor.close()
+    db.close()
