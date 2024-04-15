198c51147c8b461a09fa714b27535c4371812357
IgweEmmanuel <chigozieigweemmanuel@gmail.com>
2024-04-15 19:19:43 +0000

    filter2

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,37 +1,37 @@
 #!/usr/bin/python3
 """
-This script filters the table row
+This script takes in an argument and
+displays all values in the states
 """
-
 
 import MySQLdb
 from sys import argv
 
 if __name__ == '__main__':
     """
-    This is the filtering section
-    of the code
+    Access to the database and get the states
+    from the database.
     """
-    username = argv[1]
-    password = argv[2]
-    database = argv[3]
 
-    db = MySQLdb.connect(
-                        host='localhost',
-                        user=username, port=3306,
-                        passwd=password,
-                        db=database)
+    db = MySQLdb.connect(host="localhost", user=argv[1], port=3306,
+                         passwd=argv[2], db=argv[3])
 
-    with db.cursor() as cursor:
-        cursor.execute("SELECT * FROM states\
-                    WHERE name LIKE BINARY %(name)s\
-                    ORDER BY states.id ASC", {'name':argv[4]})
+    with db.cursor() as cur:
+        cur.execute("""
+            SELECT
+                *
+            FROM
+                states
+            WHERE
+                name LIKE BINARY %(name)s
+            ORDER BY
+                states.id ASC
+        """, {
+            'name': argv[4]
+        })
 
-        states = cursor.fetchall()
+        rows = cur.fetchall()
 
-        if states is not None:
-            for state in states:
-                print(state)
-
-        cursor.close()
-        db.close()
+    if rows is not None:
+        for row in rows:
+            print(row)
