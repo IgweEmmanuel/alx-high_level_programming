180c31c238939b4770f896c671c718d1154b1e86
IgweEmmanuel <chigozieigweemmanuel@gmail.com>
2024-04-26 04:35:18 +0000

    I got you

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #Request and response
-curl -sL -X PUT -H "Origint: You got me!" -d "user_id=98" 0.0.0.0:5000/catch_me
+curl -sL -X PUT -H "User-Agent:You got me!" -d "user_id=98" 0.0.0.0:5000/catch_me
