239f541e2a3f27464c75543d309316899e46519c
IgweEmmanuel <chigozieigweemmanuel@gmail.com>
2024-04-27 03:55:28 +0000

    check status

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,17 +1,17 @@
 #!/usr/bin/python3
-# Request to a server using urllib
-
-
+"""Request to a server using urllib"""
 import urllib
 
 
-url = "https://alx-intranet.hbtn.io/status"
+if __name__ == '__main__':
 
-with urllib.request.urlopen(url) as response:
-    output = response.read()
-    utf8 = output.decode('utf-8')
+    url = urllib.request.Request("https://alx-intranet.hbtn.io/status")
 
-print("Body response:")
-print("\t- type:", type(output))
-print("\t- content", output)
-print("\t- utf8 content:", utf8)
+    with urllib.request.urlopen(url) as response:
+        output = response.read()
+        utf8 = output.decode('utf-8')
+
+    print("Body response:")
+    print("\t- type:", type(output))
+    print("\t- content", output)
+    print("\t- utf8 content:", utf8)
