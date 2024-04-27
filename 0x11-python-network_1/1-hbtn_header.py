f31bbaf1e7f777c24dbb95743d8195210ab79e1b
IgweEmmanuel <chigozieigweemmanuel@gmail.com>
2024-04-27 05:23:36 +0000

    header

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -5,7 +5,7 @@ import sys
 
 
 if __name__ == "__main__":
-    if (len(sys.argv) != 2):
+    if len(sys.argv != 2):
         sys.exit(1)
 
     url_args = sys.argv[1]
@@ -13,7 +13,9 @@ if __name__ == "__main__":
     if not url_args.startswith('http://') and not url_args.startswith('https://'):
         sys.exit(1)
 
-    with urllib.request.urlopen(url_args) as response:
+    url = urllib.request.Request(url_args)
+
+    with urllib.request.urlopen(url) as response:
         value = response.headers.get('X-Request-Id')
         if value:
             print(value)
