51b71c8f2a5f9984c282d47eda85f5f5bfd5d6ad
IgweEmmanuel <chigozieigweemmanuel@gmail.com>
2024-04-27 05:01:03 +0000

    status

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -5,7 +5,6 @@ import urllib
 
 if __name__ == '__main__':
     url = urllib.request.Request("https://alx-intranet.hbtn.io/status")
-
     with urllib.request.urlopen(url) as response:
         output = response.read()
         utf8 = output.decode('utf-8')
