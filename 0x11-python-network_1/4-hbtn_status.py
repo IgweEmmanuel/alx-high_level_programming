dda0e0f3d61225b0d3cf2165ff3e9cfc53d554f0
IgweEmmanuel <chigozieigweemmanuel@gmail.com>
2024-04-27 04:36:45 +0000

    status

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 """Use request module to request to url"""
-import request
+import requests
 
 
 if __name__ == "__main__":
     url = "https://alx-intranet.hbtn.io/status"
-    res = request.get(url)
+    res = requests.get(url)
     print("Body response:")
     print("\t -type: ", type(res.text))
     print("\t -content: ", res.text)   
