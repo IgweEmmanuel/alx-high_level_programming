ee513f6ff7acc0fb41ae56fb460ae8f01655d2a7
IgweEmmanuel <chigozieigweemmanuel@gmail.com>
2024-04-30 22:35:27 +0000

    response

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -5,7 +5,7 @@ import urllib.request
 
 if __name__ == "__main__":
     """This prints reads the url response using with"""
-    url = "https://alx-intranet.hbtn.io/status"
+    url = urllib.request.Request("https://alx-intranet.hbtn.io/status")
     with urllib.request.urlopen(url) as response:
         output = response.read()
         utf8 = output.decode('utf-8')
