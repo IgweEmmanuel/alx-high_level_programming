bdffd3377c473e74e0f198a8eb66a0542034f11d
IgweEmmanuel <chigozieigweemmanuel@gmail.com>
2024-05-01 05:01:50 +0000

    header

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -3,17 +3,18 @@
 import urllib.request
 import sys
 
+if len(sys.argv) != 2:
+    print("Usage: python script.py <URL>")
+    sys.exit(1)
 
-if __name__ == "__main__":
-    if len(sys.argv != 2):
-        sys.exit(1)
+url = sys.argv[1]
 
-    url_args = sys.argv[1]
-    url = urllib.request.Request(url_args)
-
+try:
     with urllib.request.urlopen(url) as response:
-        value = response.headers.get("X-Request-Id")
-        if value:
-            print(value)
+        x_request_id = response.headers.get('X-Request-Id')
+        if x_request_id:
+            print(x_request_id)
         else:
-            sys.exit(1)
+            print("X-Request-Id header not found in the response.")
+except urllib.error.URLError as e:
+    print("Error:", e.reason)
