69f2158709398c665d70e0674a8cc86d6d04695a
IgweEmmanuel <chigozieigweemmanuel@gmail.com>
2024-05-21 14:49:54 +0000

    readme js file

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -2,11 +2,11 @@
 // This displays the content of a file
 
 const fs = require('node:fs');
-const { resolve } = require('path');
+//const { resolve } = require('path');
 const [, , filepath] = process.argv;
-const newpath = resolve(process.cwd(), filepath);
+//const newpath = resolve(process.cwd(), filepath);
 
-fs.readFile(newpath, 'utf-8', (err, data) => {
+fs.readFile(filepath, 'utf-8', (err, data) => {
   if (err) {
     console.error(err);
     return;
