9f5fc0013231dda14ec98e08a5bf12924500dc58
IgweEmmanuel <chigozieigweemmanuel@gmail.com>
2024-05-29 13:17:38 +0000

    add content of hello transcription in div element

--- 0x15-javascript-web_jquery/9-script.js
+++ 0x15-javascript-web_jquery/9-script.js
@@ -1,1 +1,15 @@
-https://hellosalut.stefanbohacek.dev/?lang=fr
+(function ($) {
+  $(document).ready(function () {
+    const urlLink = 'https://hellosalut.stefanbohacek.dev/?lang=fr';
+    $.ajax({
+      url: urlLink,
+      dataType: 'json',
+      success: function (data) {
+        $('#hello').text(data.hello);
+      },
+      error: function (error) {
+        console.error('Error fetching data: ', error);
+      }
+    });
+  });
+})(window.jQuery);
