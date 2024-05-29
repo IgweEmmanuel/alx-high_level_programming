a53333ed069d85ed64fa38ce78565f07d7967dab
IgweEmmanuel <chigozieigweemmanuel@gmail.com>
2024-05-29 13:58:06 +0000

    add, remove and clear elements of a list(ul)

---
+++ 0x15-javascript-web_jquery/101-script.js
@@ -0,0 +1,13 @@
+(function ($) {
+  $(document).ready(function () {
+    $('#add_item').click(function () {
+      $('.my_list').append('<li>Item</li>');
+    });
+    $('#remove_item').click(function () {
+      $('.my_list').children().last().remove();
+    });
+    $('#clear_list').click(function () {
+      $('.my_list').empty();
+    });
+  });
+})(window.jQuery);
